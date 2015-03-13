247d604f109fe01940691b782556ecb4a67a0d44
dxrodri <dxrodri@gmail.com>
2015-03-13 05:32:43 -0500

    Updating site4

--- public/content/finance/series3/index.md
+++
@@ -1,6 +0,0 @@
----
-title: "Series 3"
----
-
-Successfully passed the Series 3 exam in December 2009. 
-
--- public/layouts/resume/genp.bat
+++
@@ -1,1 +0,0 @@
-node "C:\Program Files\nodejs\node_modules/markdown-resume/bin/md2resume" --pdf single.md
